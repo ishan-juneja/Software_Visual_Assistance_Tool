f8241a9bb6e645901e122d13bafd98022e413f41
Ishan Juneja <ishanjuneja@Ishans-MacBook-Pro-2.local>
2025-11-10 18:49:39 -0800

    Added react logic with states to front end

--- frontend/src/components/UploadPanel.tsx
+++ frontend/src/components/UploadPanel.tsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
 
-function handleDragOver(e: React.DragEvent<HTMLDivElement>){
-    e.preventDefault();
-    console.log("dragging over", e.target)
-}
 
-function handleDrop(e: React.DragEvent<HTMLDivElement>){
-    e.preventDefault();
-    const files = Array.from(e.dataTransfer.files);
-    console.log("dropped files: ", )
-}
 
 export default function DropArea(){
+    const [isDragging, setIsDragging] = useState(false)
+    const [files, setFiles] = useState<File[]>([]);
+
+
+    function handleDragOver(e: React.DragEvent<HTMLDivElement>){
+        e.preventDefault();
+        console.log("dragging over", e.target)
+        setIsDragging(true);
+    }   
+
+    function handleDrop(e: React.DragEvent<HTMLDivElement>){
+        e.preventDefault();
+        const dropped = Array.from(e.dataTransfer.files);
+        console.log("dropped files: ", )
+        setFiles(dropped)
+        setIsDragging(false);
+    }
+
     return(
         <div
             onDragOver={handleDragOver} // call our functions
